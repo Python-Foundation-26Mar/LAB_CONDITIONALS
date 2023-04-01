9c3f21d5f0ebe878638d0d57076d9fd3bb46ab65
Zeinb Al Nasser <117629744+Zeinb1416@users.noreply.github.com>
2023-04-02 02:28:55 +0300

    Add files via upload
    Conditional Lab

---
+++ Conditional Lab.py
@@ -0,0 +1,14 @@
+Movie_Name = "The Marvels"
+Rating_Movie = 3
+Popularity_Score = 72.65
+
+if Rating_Movie >=4 and Popularity_Score>80:
+    print("Highly Recommended")
+elif Rating_Movie >=3 and Popularity_Score>70:
+    print("I recommended it. It is good")
+elif Rating_Movie <=2 and Popularity_Score>60:
+    print("You Should Check it out")
+else:
+    Rating_Movie <=2 and Popularity_Score<50
+    print("Do not Watch it. It is a waste of time")
+
