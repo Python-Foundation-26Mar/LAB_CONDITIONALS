2feb123e41177a0ba918ac43ae05953268f8cb09
faisal abdullah <faisal3768@gmail.com>
2023-04-02 00:24:11 +0300

    lab_3

---
+++ lab_Conditionals.py
@@ -0,0 +1,13 @@
+Movie="Big_Brother"
+Rating= 3
+PopularityScore= 72.65
+
+
+if Rating >= 4 and  PopularityScore >80:
+    print("Highly Recommended")
+elif Rating >= 3 and  PopularityScore >70:
+    print("I recommended it . It is good")
+elif Rating <= 2 and  PopularityScore >60:
+    print("ou should check it out!")
+elif Rating <= 2 and  PopularityScore <50:
+    print("Don't watch it. It is a waste of time")
