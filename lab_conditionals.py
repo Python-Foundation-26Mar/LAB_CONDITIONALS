8973b3c2987e6e8f6062a5effe13145eb83f74b8
Jannat-142 <129064948+Jannat-142@users.noreply.github.com>
2023-04-02 07:44:51 +0300

    Add files via upload

---
+++ lab_conditionals.py
@@ -0,0 +1,13 @@
+movie_name= "smile"
+movie_rate :int = 3
+movie_score :float = 72.65
+
+if movie_rate >= 4 and movie_score >80:
+    print("Highly recommended")
+elif movie_rate >= 3 and movie_score >70:
+    print("I recommended it . It is good")
+elif movie_rate <= 2 and movie_score >60:
+    print("You should check it out!")
+else:
+    print("Don't watch it. It is a waste of time")
+
